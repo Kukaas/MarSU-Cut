2fc49fc73c32d0780eb40d383cfcde170bc2608a
Roniel <roniellora8@gmail.com>
2024-08-09 21:28:00 +0800

    created the component for productions

--- src/components/components/dash-components/admin/tables/Productions.jsx
+++ src/components/components/dash-components/admin/tables/Productions.jsx
@@ -1,11 +1,404 @@
-
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { Tooltip } from "antd";
+import axios from "axios";
+import { ChevronDownIcon, PlusCircle } from "lucide-react";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const Productions = () => {
-  return (
-    <div>
-        <h1>Productions</h1>
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [sorting, setSorting] = useState([]);
+  const [columnFilters, setColumnFilters] = useState({});
+  const [columnVisibility, setColumnVisibility] = useState({});
+  const [rowSelection, setRowSelection] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const [selectedProduction, setSelectedProduction] = useState(null);
 
-export default Productions
+  const fetchProductions = async () => {
+    try {
+      setLoading(true);
+      const res = await axios.get(
+        "https://marsu.cut.server.kukaas.tech/api/v1/production/all"
+      );
+
+      const data = res.data.productions;
+      if (res.status === 200) {
+        setData(data);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductions();
+  }, []);
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    setDeleteLoading(true);
+
+    try {
+      const res = await axios.delete(
+        `https://marsu.cut.server.kukaas.tech/api/v1/production/delete/${selectedProduction._id}`
+      );
+
+      if (res.status === 200) {
+        fetchProductions();
+        setOpenDeleteDialog(false);
+        toast.success(`Produxtion with ID ${selectedProduction._id} deleted.`, {
+          action: {
+            label: "Ok"
+          }
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setDeleteLoading(false);
+    }
+  };
+
+  const columns = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label="Select all"
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label="Select row"
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "productType",
+      header: "Product Type",
+    },
+    {
+      accessorKey: "size",
+      header: "Size",
+    },
+    {
+      accessorKey: "level",
+      header: "Level",
+    },
+    {
+      accessorKey: "productionDateFrom",
+      header: "Production Date From",
+      cell: ({ row }) => {
+        const date = new Date(row.getValue("productionDateFrom"));
+        return date.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+      },
+    },
+    {
+      accessorKey: "productionDateTo",
+      header: "Production Date To",
+      cell: ({ row }) => {
+        const date = new Date(row.getValue("productionDateTo"));
+        return date.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        });
+      },
+    },
+    {
+      accessorKey: "quantity",
+      header: "Quantity",
+    },
+    {
+      accessorKey: "rawMaterialsUsed",
+      header: "Raw Materials Used",
+      cell: ({ row }) => (
+        <ul>
+          {Array.isArray(row.original.rawMaterialsUsed) ? (
+            row.original.rawMaterialsUsed.map((rawMaterial) => (
+              <li key={rawMaterial.type}>
+                {rawMaterial.type} - {rawMaterial.quantity}
+              </li>
+            ))
+          ) : (
+            <li>No raw materials used</li>
+          )}
+        </ul>
+      ),
+    },
+    {
+      id: "actions",
+      header: "Actions",
+      cell: ({ row }) => {
+        const production = row.original;
+        return (
+          <div className="flex space-x-2">
+            <Tooltip title="Delete product">
+              <Dialog
+                open={openDeleteDialog}
+                onOpenChange={setOpenDeleteDialog}
+              >
+                <DialogTrigger asChild>
+                  <Button
+                    variant="destructive"
+                    size="sm"
+                    onClick={() => {
+                      setSelectedProduction(production);
+                      setOpenDeleteDialog(true);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Delete Raw Material</DialogTitle>
+                    <DialogDescription>
+                      Are you sure you want to delete this raw material?
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="flex items-center justify-end gap-2">
+                    <DialogClose>
+                      <Button variant="outline">Cancel</Button>
+                    </DialogClose>
+                    <Button
+                      variant="destructive"
+                      onClick={(event) => handleDelete(event)}
+                    >
+                      {deleteLoading ? (
+                        <span className="loading-dots">Deleting</span>
+                      ) : (
+                        "Delete"
+                      )}
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </Tooltip>
+          </div>
+        );
+      },
+    },
+  ];
+
+  const table = useReactTable({
+    data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      pagination: {
+        pageIndex: currentPage,
+        pageSize: 5,
+      },
+    },
+  });
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 0));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => {
+      const maxPage = table.getPageCount() - 1;
+      return Math.min(prevPage + 1, maxPage);
+    });
+  };
+
+  return (
+    <div className="overflow-x-auto">
+      <div className="w-full p-4 h-screen">
+        <div className="flex items-center py-4 justify-between">
+          <Input
+            placeholder="Filter by product type..."
+            value={table.getColumn("")?.getFilterValue() || ""}
+            onChange={(event) =>
+              table.getColumn("")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+          <Tooltip title="Add new production">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="default" className="m-2">
+                  <PlusCircle size={20} className="mr-2" />
+                  New Production
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Add a new production</DialogTitle>
+                  <DialogDescription>
+                    Click submit when you&apos;re done.
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </Tooltip>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Columns <ChevronDownIcon className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    className="capitalize"
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <div className="rounded-md border">
+          {loading ? (
+            <div className="p-4">Loading...</div>
+          ) : (
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow
+                      key={row.id}
+                      data-state={row.getIsSelected() && "selected"}
+                    >
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell
+                      colSpan={table.getAllColumns().length}
+                      className="h-24 text-center"
+                    >
+                      No results.
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          )}
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handlePreviousPage}
+              disabled={currentPage === 0}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handleNextPage}
+              disabled={currentPage >= table.getPageCount() - 1}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Productions;
