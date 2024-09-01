c77c97e76d32d8b8c50e8465e2f97a0126b5455d
Roniel <roniellora8@gmail.com>
2024-09-01 23:57:17 +0800

    mofify the select field to be a scroll area

--- src/components/components/custom-components/SelectField.jsx
+++ src/components/components/custom-components/SelectField.jsx
@@ -1,16 +1,20 @@
+import React from "react";
 import { FormControl, FormItem, FormLabel, FormMessage } from "../../ui/form";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "../../ui/select";
 import PropTypes from "prop-types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 
 const SelectField = ({ field, label, options, placeholder, onValueChange }) => {
+  const hasManyOptions = options.length > 2; // Adjust the threshold as needed
+
   return (
     <FormItem>
       <FormLabel>{label}</FormLabel>
@@ -28,14 +32,27 @@ const SelectField = ({ field, label, options, placeholder, onValueChange }) => {
             <SelectValue placeholder={placeholder} />
           </SelectTrigger>
           <SelectContent>
-            <SelectGroup>
-              <SelectLabel>{label}</SelectLabel>
-              {options.map((option) => (
-                <SelectItem key={option} value={option}>
-                  {option}
-                </SelectItem>
-              ))}
-            </SelectGroup>
+            {hasManyOptions ? (
+              <ScrollArea className="h-72 p-3">
+                <SelectGroup>
+                  {options.map((option) => (
+                    <React.Fragment key={option}>
+                      <SelectItem value={option}>{option}</SelectItem>
+                      <Separator className="my-2" />
+                    </React.Fragment>
+                  ))}
+                </SelectGroup>
+              </ScrollArea>
+            ) : (
+              <SelectGroup className="p-3">
+                {options.map((option) => (
+                  <React.Fragment key={option}>
+                    <SelectItem value={option}>{option}</SelectItem>
+                    <Separator className="my-2" />
+                  </React.Fragment>
+                ))}
+              </SelectGroup>
+            )}
           </SelectContent>
         </Select>
       </FormControl>
